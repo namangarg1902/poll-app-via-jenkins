2cc958fd6b0a6bfafad56fccc84a8c8243d1b187
root <root@ip-172-31-46-5.ap-south-1.compute.internal>
2023-07-22 06:20:49 +0000

    Adding Dockerfile

--- pollme/settings.py
+++ pollme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x*za6xf&_80ofdpae!yzq61g9ffikkx9$*iygbl$j7rr4wlf8t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.233.110.239']
 
 
 # Application definition
